f86d5fa788d4b817ad577c88a3d43ca7d43b12b2
Klayang <68698886+Klayang@users.noreply.github.com>
2023-06-26 00:15:41 +0800

    Add files via upload

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -13,52 +13,57 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws IOException {
-        if (args.length == 0) GitletException.handleException("Please enter a command.");
-        String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                init(args);
-                break;
-            case "add":
-                add(args);
-                break;
-            case "commit":
-                commit(args);
-                break;
-            case "rm":
-                rm(args);
-                break;
-            case "log":
-                log(args);
-                break;
-            case "global-log":
-                globalLog(args);
-                break;
-            case "find":
-                find(args);
-                break;
-            case "status":
-                status(args);
-                break;
-            case "checkout":
-                checkout(args);
-                break;
-            case "branch":
-                branch(args);
-                break;
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) GitletException.handleException("Please enter a command.");
+            String firstArg = args[0];
+            switch (firstArg) {
+                case "init":
+                    init(args);
+                    break;
+                case "add":
+                    add(args);
+                    break;
+                case "commit":
+                    commit(args);
+                    break;
+                case "rm":
+                    rm(args);
+                    break;
+                case "log":
+                    log(args);
+                    break;
+                case "global-log":
+                    globalLog(args);
+                    break;
+                case "find":
+                    find(args);
+                    break;
+                case "status":
+                    status(args);
+                    break;
+                case "checkout":
+                    checkout(args);
+                    break;
+                case "branch":
+                    branch(args);
+                    break;
 
-            case "rm-branch":
-                rmBranch(args);
-                break;
+                case "rm-branch":
+                    rmBranch(args);
+                    break;
 //            case "reset":
 //                reset(args);
 //                break;
 //            case "merge":
 //                merge(args);
 //                break;
-            default:
-                GitletException.handleException("No command with that name exists.");
+                default:
+                    GitletException.handleException("No command with that name exists.");
+            }
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
