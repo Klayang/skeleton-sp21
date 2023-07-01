34d4f5c36e7ea803b6dffb1c0ac4ed48447f0fca
Klayang <68698886+Klayang@users.noreply.github.com>
2023-07-01 18:46:14 +0800

    Add files via upload

--- proj2/gitlet/Commands.java
+++ proj2/gitlet/Commands.java
@@ -397,11 +397,11 @@ public class Commands {
         newContent += headContent;
         newContent += "=======\n";
         newContent += branchContent;
-        newContent += ">>>>>>>";
+        newContent += ">>>>>>>\r\n";
         Utils.writeContents(currentFile, newContent);
         add(new String[]{"add", fileName});
-        String contents = Utils.readContentsAsString(currentFile);
-        System.out.print(contents);
+//        String contents = Utils.readContentsAsString(currentFile);
+//        System.out.print(contents);
     }
 
     /**
